3e7b516dbc971d32efe082748b7a53cf203a3986
Kim su-hwan <sshkim1013@naver.com>
2025-02-26 16:02:34 +0900

    feat: add application properties for JWT
    - Added jwt secret key
    - Fixed TodoRepository query issue

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -15,10 +15,9 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-//    @Query("SELECT t FROM Todo t " +
-//            "LEFT JOIN FETCH t.user " +
-//            "WHERE t.id = :todoId")
-    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user " +
+            "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
